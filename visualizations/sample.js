c31907717d7dc46af9791670a35760295f536583
aashshah_crest <aash.shah@crestdatasys.com>
2023-08-17 11:57:10 +0530

    Adding sample js.

---
+++ visualizations/sample.js
@@ -0,0 +1,25 @@
+// Use Looker's Visualization API
+looker.plugins.visualizations.add({
+    id: 'display_count',
+    label: 'Display Count',
+    create: function(element, config) {
+      // Create a container for the count value
+      this.container = element.appendChild(document.createElement("div"));
+      this.container.setAttribute("id", "count-container");
+      this.container.style.fontSize = "24px";
+      this.container.style.fontWeight = "bold";
+      this.container.style.textAlign = "center";
+    },
+
+    updateAsync: function(data, element, config, queryResponse, details, done) {
+      // Calculate the count value from the data
+      const count = data.length;
+  
+      // Display the count value in the container
+      this.container.textContent = `Count: ${count}`;
+  
+      // Signal the completion of rendering
+      done();
+    }
+  });
+  
