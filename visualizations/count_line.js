fce4e8a1326ca1a6441f4d82accb1770aa5da377
aashshah_crest <aash.shah@crestdatasys.com>
2023-08-17 15:08:44 +0530

    custom vis

--- visualizations/count_line.js
+++ visualizations/count_line.js
@@ -1,46 +1,56 @@
-// Use Looker's Visualization API
 looker.plugins.visualizations.add({
-    id: 'line-chart-below-value',
-    label: 'Line Chart Below Value',
-    create: function(element, config) {
-      // Create a container for the count value and line chart
-      this.container = element.appendChild(document.createElement("div"));
-      this.container.setAttribute("id", "visualization-container");
-  
-      // Apply styling to the container
-      this.container.style.display = "flex";
-      this.container.style.flexDirection = "column";
-      this.container.style.alignItems = "center";
+  id: 'dynamic-field-selection-visualization',
+  label: 'Dynamic Field Selection Visualization',
+  options: {
+    selectedDimension: {
+      type: 'string',
+      label: 'Select Dimension',
+      display: 'select',
+      values: "{{ dimensions }}", // Looker will populate the available dimensions here
+      default: 'dimension_name'
     },
-    updateAsync: function(data, element, config, queryResponse, details, done) {
-      // Assuming the queryResponse contains a measure or dimension named "record_count"
-      const countField = "record_count";
-  
-      // Retrieve the count value from the first row of the data
-      const count = data[0][queryResponse.fields[countField].name].value;
-  
-      // Display the count value in a paragraph
-      const countParagraph = document.createElement("p");
-      countParagraph.textContent = `Count: ${count}`;
-      this.container.appendChild(countParagraph);
-  
-      // Create a canvas element for the line chart
-      const canvas = document.createElement("canvas");
-      canvas.setAttribute("id", "line-chart");
-      this.container.appendChild(canvas);
-  
-      // Assuming you have data for the line chart (x and y values)
-      const lineData = [
-        { x: 1, y: 10 },
-        { x: 2, y: 20 },
-      ];
-  
-      // Draw the line chart below the count value
-      const ctx = canvas.getContext("2d");
-      // Use ctx to draw your line chart below the count value
-  
-      // Signal the completion of rendering
-      done();
+    selectedMeasure: {
+      type: 'string',
+      label: 'Select Measure',
+      display: 'select',
+      values: "{{ measures }}", // Looker will populate the available measures here
+      default: 'measure_name'
     }
-  });
-  
+  },
+  create: function(element, config) {
+    this.container = element.appendChild(document.createElement("div"));
+    this.container.setAttribute("id", "visualization-container");
+
+    // Apply styling to the container
+    this.container.style.display = "flex";
+    this.container.style.alignItems = "center";
+  },
+  updateAsync: function(data, element, config, queryResponse, details, done) {
+    // Use Looker's options to get dynamic field selections
+    const selectedDimension = config.selectedDimension;
+    const selectedMeasure = config.selectedMeasure;
+
+    // Retrieve the dimension and measure values from the query response
+    const dimensionValue = data[0][queryResponse.fields[selectedDimension].name].value;
+    const measureValue = data[0][queryResponse.fields[selectedMeasure].name].value;
+
+    // Display the selected dimension and measure values
+    const dynamicFieldsDiv = document.createElement("div");
+    dynamicFieldsDiv.style.display = "flex";
+    dynamicFieldsDiv.style.alignItems = "center";
+
+    const dimensionSpan = document.createElement("span");
+    dimensionSpan.textContent = `Dimension: ${dimensionValue}`;
+    dynamicFieldsDiv.appendChild(dimensionSpan);
+
+    const measureSpan = document.createElement("span");
+    measureSpan.textContent = `Measure: ${measureValue}`;
+    measureSpan.style.marginLeft = "10px";
+    dynamicFieldsDiv.appendChild(measureSpan);
+
+    this.container.appendChild(dynamicFieldsDiv);
+
+    // Signal the completion of rendering
+    done();
+  }
+});
