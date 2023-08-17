6270642d33c49f23c1437c1771b08fbe90dad69b
Aash Shah <aash.shah@crestdatasys.com>
2023-08-17 10:06:00 +0000

    modified

--- visualizations/count_line.js
+++ visualizations/count_line.js
@@ -3,7 +3,7 @@ looker.plugins.visualizations.add({
     label: 'Value and Text Visualization',
     options: {
       selectedValue: {
-        type: 'string',
+        type: 'number',
         label: 'Select Value Field',
         display: 'select',
         values: "{{ measures }}", // Looker will populate the available measures here
@@ -18,7 +18,7 @@ looker.plugins.visualizations.add({
     create: function(element, config) {
       this.container = element.appendChild(document.createElement("div"));
       this.container.setAttribute("id", "visualization-container");
-  
+
       // Apply styling to the container
       this.container.style.textAlign = "center";
     },
@@ -26,25 +26,24 @@ looker.plugins.visualizations.add({
       // Use Looker's options to get dynamic field selections
       const selectedValueField = config.selectedValue;
       const customText = config.customText;
-  
+
       // Retrieve the selected value from the query response
       const selectedValue = data[0][queryResponse.fields[selectedValueField].name].value;
-  
+
       // Display the selected value and custom text
       const valueTextDiv = document.createElement("div");
-  
+
       const valueParagraph = document.createElement("p");
       valueParagraph.textContent = `Value: ${selectedValue}`;
       valueTextDiv.appendChild(valueParagraph);
-  
+
       const customTextParagraph = document.createElement("p");
       customTextParagraph.textContent = `Custom Text: ${customText}`;
       valueTextDiv.appendChild(customTextParagraph);
-  
+
       this.container.appendChild(valueTextDiv);
-  
+
       // Signal the completion of rendering
       done();
     }
   });
-  
