3272ef132d4dbb20409fbe9efdeaaa86e4e95da1
aashshah_crest <aash.shah@crestdatasys.com>
2023-08-17 16:03:03 +0530

    adding the custom vis

--- visualizations/count_line.js
+++ visualizations/count_line.js
@@ -1,19 +1,29 @@
 // Use Looker's Visualization API
 looker.plugins.visualizations.add({
-    id: 'today-yesterday-visualization',
-    label: 'Today vs Yesterday Visualization',
+    id: 'today-yesterday-chart-visualization',
+    label: 'Today vs Yesterday Chart Visualization',
     options: {
       selectedValue: {
         type: 'number',
-        label: 'Select a Value'
+        label: 'Select a Value',
+        default: 0
       }
     },
     create: function(element, config) {
+      // Create a container for the visualization
       this.container = element.appendChild(document.createElement("div"));
       this.container.setAttribute("id", "visualization-container");
+      this.container.style.position = "relative";
       this.container.style.textAlign = "center";
       this.container.style.padding = "20px";
+  
+      // Create a container for the line chart
+      this.chartContainer = this.container.appendChild(document.createElement("div"));
+      this.chartContainer.style.width = "100%";
+      this.chartContainer.style.height = "300px";
+      this.chartContainer.style.marginTop = "20px";
     },
+
     updateAsync: function(data, element, config, queryResponse, details, done) {
       // Retrieve the selected dynamic value from Looker's options
       const selectedValue = config.selectedValue;
@@ -27,7 +37,18 @@ looker.plugins.visualizations.add({
       textParagraph.textContent = "Today vs Yesterday";
       this.container.appendChild(textParagraph);
   
-      // Signal the completion of rendering
+      // Extract data for the line chart (you need to have x and y values)
+      const lineChartData = [
+        { x: "Today", y: selectedValue },
+        { x: "Yesterday", y: selectedValue - 10 } // Example: value for yesterday
+      ];
+  
+      // Set up the line chart using D3.js
+      const svg = d3.select(this.chartContainer)
+        .append("svg")
+        .attr("width", "100%")
+        .attr("height", "100%");
+  
       done();
     }
   });
