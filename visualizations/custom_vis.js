8dc7d2aa5d391464a700d372b936c696d4b9a527
Aash Shah <aash.shah@crestdatasys.com>
2023-08-18 05:27:42 +0000

    updating

--- visualizations/custom_vis.js
+++ visualizations/custom_vis.js
@@ -22,13 +22,6 @@ looker.plugins.visualizations.add({
       this.textContainer.style.textAlign = "center";
       this.textContainer.style.padding = "5px";
       this.textContainer.style.fontFamily = "Arial";
-
-      // Create a container for the difference chart
-      this.chartContainer = element.appendChild(document.createElement("canvas"));
-      this.chartContainer.setAttribute("id", "difference-chart-container");
-      this.chartContainer.style.width = "80%";
-      this.chartContainer.style.height = "300px";
-      this.chartContainer.style.margin = "20px auto";
     },
 
     updateAsync: function(data, element, config, queryResponse, details, done) {
@@ -56,54 +49,6 @@ looker.plugins.visualizations.add({
 
       // Display the text line below the count value
       this.textContainer.textContent = "Today vs Yesterday";
-
-      // Prepare data for the difference chart
-      const dates = ['2023-08-01', '2023-08-02', '2023-08-03', '2023-08-04']; // Example dates
-      const values = [100, 120, 90, 110]; // Example values
-      
-      // Calculate the difference between today and previous day values
-      const valueDifferences = [];
-      for (let i = 1; i < values.length; i++) {
-        const difference = values[i] - values[i - 1];
-        valueDifferences.push(difference);
-      }
-
-      const chartData = {
-        labels: dates.slice(1), // Remove the first date since it doesn't have a previous day
-        datasets: [{
-          label: 'Value Differences',
-          data: valueDifferences,
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 2,
-          fill: false
-        }]
-      };
-
-      // Create the difference chart using Chart.js
-      const ctx = this.chartContainer.getContext('2d');
-      if (this.differenceChart) {
-        this.differenceChart.destroy(); // Destroy existing chart if it exists
-      }
-      this.differenceChart = new Chart(ctx, {
-        type: 'line',
-        data: chartData,
-        options: {
-          scales: {
-            x: {
-              type: 'time',
-              time: {
-                unit: 'day',
-                displayFormats: {
-                  day: 'MMM D'
-                }
-              }
-            },
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      });
 
       // Signal the completion of rendering
       done();
