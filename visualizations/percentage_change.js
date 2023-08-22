7ff73c90aa39af7b28f0f120ada834510955b6ee
Aash Shah <aash.shah@crestdatasys.com>
2023-08-22 05:37:37 +0000

    adding sparkline values

--- visualizations/percentage_change.js
+++ visualizations/percentage_change.js
@@ -1,44 +1,44 @@
 looker.plugins.visualizations.add({
-  id: 'percentage_change_for_each_date',
-  label: 'Percentage Change for Each Date',
   create: function(element, config) {
-    // Create a container for displaying the percentage change
-    this.container = element.appendChild(document.createElement("div"));
-    this.container.setAttribute("id", "percentage-change-container");
+    // Create a new DOM element to hold the sparkline chart
+    var sparklineContainer = $('<div class="sparkline-chart"></div>');
+    element.append(sparklineContainer);
 
-    // Applying Styling to the container
-    this.container.style.fontWeight = "bold";
-    this.container.style.textAlign = "center";
-    this.container.style.padding = "10px";
-    this.container.style.fontFamily = "Arial"; // Set font family to Arial
+    // Include Sparkline.js from a CDN (replace with the actual CDN URL)
+    var sparklineJsScript = document.createElement('script');
+    sparklineJsScript.src = 'URL_TO_SPARKLINE_JS_CDN'; // Replace with the actual CDN URL
+    document.head.appendChild(sparklineJsScript);
+
+    // Return the container element
+    return sparklineContainer[0];
   },
-
   updateAsync: function(data, element, config, queryResponse, details, done) {
-    // Get the date dimension and count measure names from config
-    const dateDimension = config.dimensions[0]; // Assuming only one dimension is selected
-    const countMeasure = config.measures[0];     // Assuming only one measure is selected
+    // Dummy data for demonstration (replace with your Looker data)
+    var dummyData = [
+      { sparkline_value: 10 },
+      { sparkline_value: 20 },
+      { sparkline_value: 15 },
+      { sparkline_value: 30 },
+      { sparkline_value: 25 },
+    ];
 
-    // Calculate the percentage change for each date
-    const percentageChanges = [];
-    for (const row of data) {
-      const currentDate = row[dateDimension].value;
-      const currentCount = row[countMeasure].value;
+    // Extract the data for the sparkline chart
+    var sparklineData = dummyData.map(function(row) {
+      return row["sparkline_value"];
+    });
 
-      // Find the count for yesterday (assuming your dates are sorted)
-      const previousIndex = data.findIndex(item => item[dateDimension].value === currentDate) - 1;
-      const previousCount = previousIndex >= 0 ? data[previousIndex][countMeasure].value : 0;
+    // Render the sparkline chart using Sparkline.js
+    // Assuming you've included Sparkline.js in the head of your HTML file
+    // and created a div with class "sparkline-chart" in your HTML
+    $(element)
+      .find('.sparkline-chart')
+      .sparkline(sparklineData, {
+        type: 'line',
+        width: '100px', // Customize width as needed
+        height: '30px' // Customize height as needed
+      });
 
-      // Calculate the percentage change
-      const percentageChange = ((currentCount - previousCount) / previousCount) * 100;
-
-      // Display the percentage change for each date
-      percentageChanges.push(`${currentDate}: ${percentageChange.toFixed(2)}%`);
-    }
-
-    // Display the percentage change values in the container
-    this.container.textContent = percentageChanges.join('\n');
-
-    // Signal the completion of rendering
+    // Call the 'done' function to signal that the rendering is complete
     done();
   }
 });
