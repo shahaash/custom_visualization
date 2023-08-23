f93602b68f7da043babac3b899f8bc924bed3eef
Aash Shah <aash.shah@crestdatasys.com>
2023-08-23 05:32:53 +0000

    code changes

--- visualizations/percentage_change.js
+++ visualizations/percentage_change.js
@@ -1,44 +1,37 @@
 looker.plugins.visualizations.add({
-  id: 'percentage_change_for_each_date',
-  label: 'Percentage Change for Each Date',
-  create: function(element, config) {
-    // Create a container for displaying the percentage change
-    this.container = element.appendChild(document.createElement("div"));
-    this.container.setAttribute("id", "percentage-change-container");
+    create: function (element, config) {
+        var chart = document.createElement('table');
+        chart.id = 'custom-table-chart';
+        element.appendChild(chart);
+        var style = document.createElement('style');
+        style.innerHTML = `table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
 
-    // Applying Styling to the container
-    this.container.style.fontWeight = "bold";
-    this.container.style.textAlign = "center";
-    this.container.style.padding = "10px";
-    this.container.style.fontFamily = "Arial"; // Set font family to Arial
-  },
-
-  updateAsync: function(data, element, config, queryResponse, details, done) {
-    // Get the date dimension and count measure names from config
-    const dateDimension = config.dimensions[0]; // Assuming only one dimension is selected
-    const countMeasure = config.measures[0];     // Assuming only one measure is selected
-
-    // Calculate the percentage change for each date
-    const percentageChanges = [];
-    for (const row of data) {
-      const currentDate = row[dateDimension].value;
-      const currentCount = row[countMeasure].value;
-
-      // Find the count for yesterday (assuming your dates are sorted)
-      const previousIndex = data.findIndex(item => item[dateDimension].value === currentDate) - 1;
-      const previousCount = previousIndex >= 0 ? data[previousIndex][countMeasure].value : 0;
-
-      // Calculate the percentage change
-      const percentageChange = ((currentCount - previousCount) / previousCount) * 100;
-
-      // Display the percentage change for each date
-      percentageChanges.push(`${currentDate}: ${percentageChange.toFixed(2)}%`);
+        }`
+        element.appendChild(style);
+    },
+    updateAsync: function (data, element, config, queryResponse, details, doneRendering) {
+        var chart = element.querySelector('#custom-table-chart');
+        chart.innerHTML = '';
+        var headerRow = document.createElement('tr');
+        for (var i of queryResponse.fields.dimensions) {
+            var th = document.createElement('th');
+            th.textContent = i.name;
+            headerRow.appendChild(th);
+        }
+        chart.appendChild(headerRow);
+        // Create table rows
+        data.forEach(function (row) {
+            var rows = document.createElement('tr');
+            Object.keys(row).forEach(function (key) {
+                var td = document.createElement('td');
+                td.textContent = row[key].value;
+                rows.appendChild(td);
+            });
+            chart.appendChild(rows);
+        });
+        element.appendChild(chart);
+        doneRendering()
     }
-
-    // Display the percentage change values in the container
-    this.container.textContent = percentageChanges.join('\n');
-
-    // Signal the completion of rendering
-    done();
-  }
 });
