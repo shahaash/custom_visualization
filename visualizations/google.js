b3e8afb4cc2d4cfa10f1fadf2339f72bc86219e9
Aash Shah <aash.shah@crestdatasys.com>
2023-08-23 13:34:00 +0000

    changes in google.js

--- visualizations/google.js
+++ visualizations/google.js
@@ -7,31 +7,19 @@ looker.plugins.visualizations.add({
     var button = document.createElement('button');
     button.innerText = 'Click Me';
 
-    // Create a modal element
-    var modal = document.createElement('div');
-    modal.classList.add('modal', 'fade');
-    modal.tabIndex = -1;
-    modal.setAttribute('role', 'dialog');
-    modal.innerHTML = `
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-body">
-            <p>Button Clicked!</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
-    `;
+    // Create a div to display a message
+    var messageDiv = document.createElement('div');
+    messageDiv.style.display = 'none'; // Initially hide it
 
-    // Add a click event listener to the button to show the modal
+    // Add a click event listener
     button.addEventListener('click', function () {
-      $(modal).modal('show');
+      // Display the message
+      messageDiv.innerText = 'Button Clicked!';
+      messageDiv.style.display = 'block';
     });
 
-    // Append the button and modal to the element
+    // Append the button and message div to the element
     element.appendChild(button);
-    element.appendChild(modal);
+    element.appendChild(messageDiv);
   },
 });
