c8f9de40cd77910e1e5394537bcd540ad92a43ef
Aash Shah <aash.shah@crestdatasys.com>
2023-08-24 10:02:02 +0000

    code changes

--- visualizations/google.js
+++ visualizations/google.js
@@ -4,48 +4,50 @@ looker.plugins.visualizations.add({
         var button = document.createElement('button');
         button.textContent = 'Click me';
 
+        // Create a popup container element
+        var popupContainer = document.createElement('div');
+        popupContainer.className = 'popup-container';
+
+        // Create a popup content element
+        var popupContent = document.createElement('div');
+        popupContent.className = 'popup-content';
+        popupContent.style.display = 'none'; // Hide initially
+
+        // Create a close button for the popup
+        var closeButton = document.createElement('button');
+        closeButton.textContent = 'Close';
+
+        // Create a message element
+        var message = document.createElement('p');
+        message.textContent = 'This is a custom popup message.';
+
+        // Append the message to the popup content
+        popupContent.appendChild(message);
+
+        // Append the close button to the popup content
+        popupContent.appendChild(closeButton);
+
         // Append the button to the element
         element.appendChild(button);
 
+        // Append the popup content to the popup container
+        popupContainer.appendChild(popupContent);
+
+        // Append the popup container to the element
+        element.appendChild(popupContainer);
+
         // Attach a click event handler to show the popup when the button is clicked
         button.addEventListener('click', function() {
-            // Create a modal container
-            var modalContainer = document.createElement('div');
-            modalContainer.className = 'modal-container';
+            popupContent.style.display = 'block'; // Show the popup
+        });
 
-            // Create the modal content
-            var modalContent = document.createElement('div');
-            modalContent.className = 'modal-content';
-
-            // Create a close button for the modal
-            var closeButton = document.createElement('button');
-            closeButton.textContent = 'Close';
-
-            // Create a message element
-            var message = document.createElement('p');
-            message.textContent = 'This is a custom popup message.';
-
-            // Append the message to the modal content
-            modalContent.appendChild(message);
-
-            // Append the close button to the modal content
-            modalContent.appendChild(closeButton);
-
-            // Append the modal content to the modal container
-            modalContainer.appendChild(modalContent);
-
-            // Append the modal container to the body
-            document.body.appendChild(modalContainer);
-
-            // Attach a click event handler to close the modal when the close button is clicked
-            closeButton.addEventListener('click', function() {
-                // Remove the modal container from the body
-                document.body.removeChild(modalContainer);
-            });
+        // Attach a click event handler to close the popup when the close button is clicked
+        closeButton.addEventListener('click', function() {
+            popupContent.style.display = 'none'; // Hide the popup
         });
     },
     updateAsync: function(data, element, config, queryResponse, details, done) {
-        // This is where you'd potentially update the button behavior based on data
+        // This is where you'd potentially update the button or popup based on data
         // Since you don't need data, this might be empty or contain some static behavior
 
         // Call done to signal rendering completion
