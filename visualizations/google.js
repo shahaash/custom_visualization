34aaa9dcd04330739d084c8c67c17ddf5048dbe7
Aash Shah <aash.shah@crestdatasys.com>
2023-08-24 09:39:49 +0000

    code changes

--- visualizations/google.js
+++ visualizations/google.js
@@ -3,51 +3,45 @@ looker.plugins.visualizations.add({
         // Create a button element
         var button = document.createElement('button');
         button.textContent = 'Click me';
-        button.className = 'btn btn-primary'; // Add Bootstrap button classes
 
-        // Attach a click event handler to show the popup
-        button.addEventListener('click', function() {
-            // Create a modal element
-            var modal = document.createElement('div');
-            modal.className = 'modal';
+        // Create a popup container element
+        var popupContainer = document.createElement('div');
+        popupContainer.className = 'popup-container';
 
-            // Create modal content
-            var modalContent = document.createElement('div');
-            modalContent.className = 'modal-content';
-            modalContent.innerHTML = `
-                <div class="modal-header">
-                    <h5 class="modal-title">Popup Title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>This is a sample popup content.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                </div>
-            `;
+        // Create a popup content element
+        var popupContent = document.createElement('div');
+        popupContent.className = 'popup-content';
+        popupContent.style.display = 'none'; // Hide initially
 
-            // Append modal content to modal
-            modal.appendChild(modalContent);
+        // Create a close button for the popup
+        var closeButton = document.createElement('button');
+        closeButton.textContent = 'Close';
 
-            // Show the modal using Bootstrap's modal API
-            $(modal).modal('show');
-
-            // Cleanup modal when it's closed
-            $(modal).on('hidden.bs.modal', function () {
-                // Remove the modal element from the DOM
-                $(modal).remove();
-            });
-        });
+        // Append the close button to the popup content
+        popupContent.appendChild(closeButton);
 
         // Append the button to the element
         element.appendChild(button);
+
+        // Append the popup content to the popup container
+        popupContainer.appendChild(popupContent);
+
+        // Append the popup container to the element
+        element.appendChild(popupContainer);
+
+        // Attach a click event handler to show the popup when the button is clicked
+        button.addEventListener('click', function() {
+            popupContent.style.display = 'block'; // Show the popup
+        });
+
+        // Attach a click event handler to close the popup when the close button is clicked
+        closeButton.addEventListener('click', function() {
+            popupContent.style.display = 'none'; // Hide the popup
+        });
     },
     updateAsync: function(data, element, config, queryResponse, details, done) {
-        // This custom visualization doesn't require data from Looker
-        // It's a standalone button and popup
+        // This is where you'd potentially update the button or popup based on data
+        // Since you don't need data, this might be empty or contain some static behavior
 
         // Call done to signal rendering completion
         done();
