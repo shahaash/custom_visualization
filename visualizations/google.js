91faf39b635a7e07e256ded867aa7018fd157bec
Aash Shah <aash.shah@crestdatasys.com>
2023-08-25 12:10:23 +0000

    updating code

--- visualizations/google.js
+++ visualizations/google.js
@@ -1,37 +1,37 @@
 looker.plugins.visualizations.add({
     create: function(element, config) {
-        // Create a search input field
-        var searchInput = document.createElement('input');
-        searchInput.type = 'text';
-        searchInput.placeholder = 'Search...';
+      // Create a search input field
+      var searchInput = document.createElement('input');
+      searchInput.type = 'text';
+      searchInput.placeholder = 'Search...';
 
-        // Create a search button
-        var searchButton = document.createElement('button');
-        searchButton.textContent = 'Search';
+      // Create a search button
+      var searchButton = document.createElement('button');
+      searchButton.textContent = 'Search';
 
-        // Create a div to display the search query
-        var queryDisplay = document.createElement('div');
-        queryDisplay.className = 'query-display';
+      // Create a div to display the search query
+      var queryDisplay = document.createElement('div');
+      queryDisplay.className = 'query-display';
 
-        // Append the input field, button, and query display div to the element
-        element.appendChild(searchInput);
-        element.appendChild(searchButton);
-        element.appendChild(queryDisplay);
+      // Append the input field, button, and query display div to the element
+      element.appendChild(searchInput);
+      element.appendChild(searchButton);
+      element.appendChild(queryDisplay);
 
-        // Attach an event listener to the search button
-        searchButton.addEventListener('click', function() {
-            // Get the search query from the input field
-            var query = searchInput.value;
+      // Attach an event listener to the search button
+      searchButton.addEventListener('click', function() {
+          // Get the search query from the input field
+          var query = searchInput.value;
 
-            // Display the search query
-            queryDisplay.textContent = 'Search Query: ' + query;
-        });
+          // Display the search query
+          queryDisplay.textContent = 'Search Query: ' + query;
+      });
     },
     updateAsync: function(data, element, config, queryResponse, details, done) {
-        // This is where you'd handle the response from Looker and update your visualization
-        // based on the search results.
+      // This is where you'd handle the response from Looker and update your visualization
+      // based on the search results.
 
-        // Call done to signal rendering completion
-        done();
+      // Call done to signal rendering completion
+      done();
     }
 });
